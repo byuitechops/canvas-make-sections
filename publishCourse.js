cb0460e8081c54fd285ff01ca231ecbde0222e6d
Danverde <gre12014@byui.edu>
2018-04-10 14:54:50 -0600

    first version of publishCourse. needs testing

--- publishCourse.js
+++ publishCourse.js
@@ -1,6 +1,22 @@
+/* eslint no-console:0 */
+
 const canvas = require('canvas-wrapper');
 const chalk = require('chalk');
 
 module.exports = (course, callback) => {
-    callback(null, course);
+    var sisID = encodeURI(`sis_course_id:${course.course_id}`),
+        putObj = {
+            'course[is_public_to_auth_users]': true
+        };
+
+    canvas.put(`/api/v1/courses/${sisID}`, putObj, (putErr, updatedCourse) => {
+        if (putErr) {
+            console.log('Error while publishing the course');
+            console.error(chalk.red(putErr));
+        } else {
+            console.log(`Successfully published ${course.updatedCourse.name}`);
+            console.log(`Course ID: ${updatedCourse.id}`);
+        }
+        callback(null, course);
+    });
 };
