0a7675c6467842a252d829c30e5752c419e91d7f
Danverde <gre12014@byui.edu>
2018-04-10 11:40:54 -0600

    stub functions in place. runs all the way through

--- copyGroups.js
+++ copyGroups.js
@@ -1,7 +1,7 @@
 const canvasCopyGroups = require('canvas-copy-groups');
 
 module.exports = (course, callback) => {
-
+    
     
     callback(null, course);
 };
---
+++ enableBP.js
@@ -0,0 +1,5 @@
+module.exports = (course, callback) => {
+    
+    
+    callback(null, course);
+};
