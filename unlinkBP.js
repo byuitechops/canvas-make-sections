e23e6307a59c6a3deb2531557db97e3c33a5b59e
Wilson <kilakal@byui.edu>
2018-04-11 09:50:17 -0600

    commit to pull

--- unlinkBP.js
+++ unlinkBP.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 
 module.exports = (course, callback) => {
     canvas.putJSON(`/api/v1/courses/sis_course_id:${course.blueprint_course_id}/blueprint_templates/default/update_associations`,
-        { 'course_ids_to_remove': [course.course_id] }, (err) => {
+        { 'course_ids_to_remove': `sis_course_id:${[course.course_id]}` }, (err) => {
             if (err) {
                 console.log(chalk.red(err.stack));
                 callback(null, course);
